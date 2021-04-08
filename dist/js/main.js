e1cc573c94e4d26aded0d035e211a200da5a7d87
Axel Knutsson <axelkknutsson@gmail.com>
2021-04-08 15:10:34 +0200

    predict button and values

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,6 @@
 async function fetchAlcoholStressed() {
   // get data from server
-  let res = await fetch('/rest/insights')
+  let res = await fetch('/api/insights')
 
   // convert list of JSON to objects
   let alcoholStressed = await res.json()
@@ -10,7 +10,7 @@ async function fetchAlcoholStressed() {
 
 async function fetchAlcoholNotStressed() {
   // get data from server
-  let res = await fetch('/rest/insights')
+  let res = await fetch('/api/insights')
   
   // convert list of JSON to objects
   let alcoholNotStressed = await res.json()
@@ -20,10 +20,43 @@ async function fetchAlcoholNotStressed() {
 
 async function fetchSpecialization() {
   // get data from server
-  let res = await fetch('/rest/insights')
+  let res = await fetch('/api/insights')
     
   // convert list of JSON to objects
   let specialization = await res.json()
     
   console.log(specialization) // debug log
 }
+
+('#predict-btn').click(async function() {
+  let ageValue = $('input[name="age"]:checked').val();
+  let genderValue = $('input[name="gender"]:checked').val();
+  let specializationValue = $('input[name="specialization"]:checked').val();
+  let workHourValue = $('input[name="workHour"]:checked').val();
+  let patientsPerDayValue = $('input[name="patientsPerDay"]:checked').val();
+  let overtimeWorkInterestValue = $('input[name="overtimeWorkInterest"]:checked').val();
+  let overtimeWorkPaidValue = $('input[name="overtimeWorkPaid"]:checked').val();
+  let sectorValue = $('input[name="sector"]:checked').val();
+
+  let predictValues = {
+    age: ageValue,
+    gender: genderValue,
+    workHour: workHourValue,
+    Specialization: specializationValue,
+    patientPerDay: patientsPerDayValue,
+    overtimeWorkInterest: overtimeWorkInterestValue,
+    overtimeWorkPaid: overtimeWorkPaidValue,
+    sector: sectorValue
+  }
+
+  let res = await fetch('/api/predict', {
+    method: 'POST',
+    body: JSON.stringify(predictValues)
+  })
+
+  let prediction = await res.json()
+
+  $('#prediction').html(`
+    Will user click this ad: <em>${prediction['will-click']}</em>
+  `)
+})
