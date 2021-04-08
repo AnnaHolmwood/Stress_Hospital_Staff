48fb4df7a37041fffcd0d8adebe62369b50db253
khalid Ibrahim <khaibr-19@outlook.com>
2021-04-08 20:29:15 +0200

    Fix prediction page, main server and some CSS.

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,62 +1,74 @@
-async function fetchAlcoholStressed() {
-  // get data from server
-  let res = await fetch('/api/insights')
+// async function fetchAlcoholStressed() {
+//   // get data from server
+//   let res = await fetch("/api/insights")
 
-  // convert list of JSON to objects
-  let alcoholStressed = await res.json()
+//   // convert list of JSON to objects
+//   let alcoholStressed = await res.json()
 
-  console.log(alcoholStressed) // debug log
-}
+//   console.log(alcoholStressed) // debug log
+// }
 
-async function fetchAlcoholNotStressed() {
-  // get data from server
-  let res = await fetch('/api/insights')
-  
-  // convert list of JSON to objects
-  let alcoholNotStressed = await res.json()
-  
-  console.log(alcoholNotStressed) // debug log
-}
+// async function fetchAlcoholNotStressed() {
+//   // get data from server
+//   let res = await fetch("/api/insights")
 
-async function fetchSpecialization() {
-  // get data from server
-  let res = await fetch('/api/insights')
-    
-  // convert list of JSON to objects
-  let specialization = await res.json()
-    
-  console.log(specialization) // debug log
-}
+//   // convert list of JSON to objects
+//   let alcoholNotStressed = await res.json()
 
-('#predict-btn').click(async function() {
-  let ageValue = $('input[name="age"]:checked').val();
-  let genderValue = $('input[name="gender"]:checked').val();
-  let specializationValue = $('input[name="specialization"]:checked').val();
-  let workHourValue = $('input[name="workHour"]:checked').val();
-  let patientsPerDayValue = $('input[name="patientsPerDay"]:checked').val();
-  let overtimeWorkInterestValue = $('input[name="overtimeWorkInterest"]:checked').val();
-  let overtimeWorkPaidValue = $('input[name="overtimeWorkPaid"]:checked').val();
-  let sectorValue = $('input[name="sector"]:checked').val();
+//   console.log(alcoholNotStressed) // debug log
+// }
+
+// async function fetchSpecialization() {
+//   // get data from server
+//   let res = await fetch("/api/insights")
+
+//   // convert list of JSON to objects
+//   let specialization = await res.json()
+
+//   console.log(specialization) // debug log
+// }
+$(".predict-btn").on("click", async function () {
+  let ageValue = $('input[name="age"]:checked').val()
+  let genderValue = $('input[name="gender"]:checked').val()
+  let specializationValue = $('input[name="specialization"]:checked').val()
+  let workHourValue = $('input[name="workHour"]:checked').val()
+  let patientsPerDayValue = $('input[name="patientsPerDay"]:checked').val()
+  let overtimeWorkInterestValue = $(
+    'input[name="overtimeWorkInterest"]:checked'
+  ).val()
+  let overtimeWorkPaidValue = $('input[name="overtimeWorkPaid"]:checked').val()
+  let sectorValue = $('input[name="sector"]:checked').val()
 
   let predictValues = {
     age: ageValue,
     gender: genderValue,
-    workHour: workHourValue,
+    workHours: workHourValue,
     Specialization: specializationValue,
     patientPerDay: patientsPerDayValue,
     overtimeWorkInterest: overtimeWorkInterestValue,
     overtimeWorkPaid: overtimeWorkPaidValue,
-    sector: sectorValue
+    sector: sectorValue,
   }
 
-  let res = await fetch('/api/predict', {
-    method: 'POST',
-    body: JSON.stringify(predictValues)
+  const res = await fetch("/api/predict", {
+    method: "POST",
+    contentType: "application/json",
+    body: JSON.stringify(predictValues),
   })
 
   let prediction = await res.json()
+  if (prediction.status === "error")
+    return $(".predictionInfo").html(`
+      <p>${prediction.massage}</p>
+    `)
 
-  $('#prediction').html(`
-    Will user click this ad: <em>${prediction['will-click']}</em>
+  $(".predictionInfo").html(`
+    <p>${prediction.massage}</p>
   `)
+
+  // vissa chart
+  // console.log(prediction)
+  // $("#prediction").html(`
+  //   Will user click this ad: <em>${prediction["will-click"]}</em>
+  // `)
 })
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from sanic import Sanic, response
 from sanic.exceptions import NotFound
 from ML_model import predict_col11, predict_col12, train_col11, train_col12
-from database import get_alcohol_stressed, get_alcohol_not_stressed, get_specialization
 
 app = Sanic(__name__)
 
@@ -12,44 +11,40 @@ train_col11()
 @app.post('/api/predict')
 async def predict_stress(req):
   values = req.json
+  if len(values) == None or len(values) == 0 or len(values) == "":
+    return response.json({"status": "error", "massage": "Checkmark should not be uncheck!"}, status=400)
+  print("request values: ", values)
   prediction = predict_col11(values['age'], values['gender'], values['Specialization'], values['workHours'], values['patientPerDay'], values['overtimeWorkInterest'], values['overtimeWorkPaid'], values['sector'])
   print('prediction2 says:', prediction)
-  return response.json(prediction)
+  return response.json({"status": "success", "massage": prediction}, status=200)
 
  
-async def predict_work(req):
-  values = req.json 
-  prediction = predict_col12(values['age'], values['gender'], values['Specialization'], values['workHours'], values['patientPerDay'], values['overtimeWorkInterest'], values['overtimeWorkPaid'], values['sector'])
-  print('prediction1 says:', prediction)
-  return response.json(prediction)
+# async def predict_work(req):
+#   values = req.json 
+#   prediction = predict_col12(values['age'], values['gender'], values['Specialization'], values['workHours'], values['patientPerDay'], values['overtimeWorkInterest'], values['overtimeWorkPaid'], values['sector'])
+#   print('prediction1 says:', prediction)
+#   return response.json(prediction)
 
-@app.get('/api/insights')
-async def get_alcohol_stressed(req):
-  alcohol_stressed = await get_alcohol_stressed()
-  return response.json(alcohol_stressed)
+# @app.get('/api/insights')
+# async def get_alcohol_stressed(req):
+#   alcohol_stressed = await get_alcohol_stressed()
+#   return response.json(alcohol_stressed)
 
-async def get_alcohol_not_stressed(req):
-  alcohol_not_stressed = await get_alcohol_not_stressed()
-  return response.json(alcohol_not_stressed)
+# async def get_alcohol_not_stressed(req):
+#   alcohol_not_stressed = await get_alcohol_not_stressed()
+#   return response.json(alcohol_not_stressed)
 
-async def get_specialization(req):
-  specialization = await get_specialization()
-  return response.json(specialization)
+# async def get_specialization(req):
+#   specialization = await get_specialization()
+#   return response.json(specialization)
 
 
 app.static('/', './dist')
-
-
-
-#404 exception
 
 @app.exception(NotFound)
 async def ignore_404s(req, err):
 
   return await response.file('./dist/index.html')
 
-
-
 if __name__ == "__main__":
-
   app.run(port=8000)
