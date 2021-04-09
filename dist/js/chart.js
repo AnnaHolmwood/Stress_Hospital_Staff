d006c9cf2f3a522edc9a8aa338f2e9b678b5af51
Anna Holmwood <anna_holmwood@hotmail.com>
2021-04-09 10:39:46 +0200

    Update about and add chart to insights

--- dist/js/chart.js
+++ dist/js/chart.js
@@ -1,16 +1,15 @@
 // Pass canvas id for Chart to work.
-let chart = new Chart('chart', {
+let chart = new Chart('chart1', {
   type: 'bar',
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green'],
+    labels: ["Sometimes", "Very Often", "Never"],
     datasets: [
       {
         order: 0,
-        label: 'Bar Dataset',
+        label: 'Stress among hospital staff',
         borderWidth: 3,
         data: [53, 39, 6],
-        // we can use HEX, rgba or simple css colors
-        backgroundColor: ["#FF5768", "#00A5E3", "#FFBF65", "#8DD7BF"]
+        backgroundColor: ["#FF5768", "#00A5E3", "#FFBF65"]
       },
     ]
   },
@@ -36,35 +35,3 @@ let chart = new Chart('chart', {
     }
   }
 })
-
-function addChartData(label, color, data) {
-  let chartDataset = chart.data.datasets[0]
-
-  chart.data.labels.push(label)
-  chartDataset.data.push(data)
-  chartDataset.backgroundColor.push(color)
-  // This step is important since each time something is added to canvas it needs to be updated as well
-  chart.update()
-}
-
-
-addChartData('Purple', 'purple', 15)
-addChartData('Orange', 'orange', 35)
-
-
-function addLineDataset(){
-  chart.data.datasets.push({
-    label: 'Line Dataset',
-    data: [90, 32, 5, 15, 45, 62],
-    type: 'line',
-    backgroundColor: ['rgba(0, 0, 0, 0)'],
-    borderColor: ['#03a5fc'],
-    order: 1,
-    pointRadius: 10,
-    pointBorderColor: 'black'
-  })
-
-  chart.update()
-}
-
-addLineDataset()
